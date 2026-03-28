24f3d08daaa73d08bf3d60163a085cbc10af285d
AbdelrahmanSamirz <shekaba@gmail.com>
2026-03-28 14:32:41 +0200

    Question01

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Assignment_Session01_AdvancedC_
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment_Session01_AdvancedC_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            //Q1: What is a generic class? Why use generics?
+            //Generic class:
+            //A class that uses a type parameter(like<T>) instead of a fixed data type, so it can work with any type
+            //Why use generics ?
+            //Reusability : write once, use with different types
+            //Type safety: fewer errors
+            //Performance: no boxing / unboxing
+            #endregion
+
+
         }
     }
 }
