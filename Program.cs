f9843925b10bed38f6d873494616bd1dfd54be72
AbdelrahmanSamirz <shekaba@gmail.com>
2026-03-28 16:39:22 +0200

    Question15

--- Program.cs
+++ Program.cs
@@ -141,6 +141,14 @@ namespace Assignment_Session01_AdvancedC_
             ////Console.WriteLine(words.Get(2) == null); // True 
 
             #endregion
+            #region Question15
+            //Q15: What is covariance? Explain the 'out' keyword.
+
+
+            //Answer : Covariance allows you to use a more derived type than originally specified.
+            //The 'out' keyword is used to indicate that a type parameter is covariant,
+            //meaning it can be substituted with a more derived type
+            #endregion
         }
     }
 }
